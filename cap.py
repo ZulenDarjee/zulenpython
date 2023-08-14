655456b17b5036b7e3b72564a9aa02b6e7654e7b
ZulenDarjee <130635762+ZulenDarjee@users.noreply.github.com>
2023-08-14 00:38:33 -0700

    Add files via upload

---
+++ cap.py
@@ -0,0 +1,7 @@
+def cap_text(text):
+    '''
+    Input the string 
+    output the capitalized string
+    '''
+    return text.title()
+#.title()
---
+++ simple1.py
@@ -0,0 +1,16 @@
+'''
+A very simple script
+'''
+def myfunction():
+    '''
+    A simple function
+    '''
+    #printing and declaring the numbers
+    first_num = 1
+    second_num = 2
+    print(first_num)
+    print(second_num)
+#gave an extra line here 
+myfunction()
+
+
